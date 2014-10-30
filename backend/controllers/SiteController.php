2eeed08a8d46ccf210595dbd745d670ce8f96ea2
Budi (bugan) <budi@ebizu.com>
2014-10-30 08:32:25 +0700

    remove unused codes

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -5,7 +5,6 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use common\models\LoginForm;
-use api\modules\v1\models\Country;
 use yii\filters\VerbFilter;
 
 /**
@@ -64,12 +63,6 @@ class SiteController extends Controller
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
-
-        $country = new Country();
-        $country->code = 'ID';
-        $country->name = 'Indonesia';
-        $country->population = 1000;
-        $country->save();
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
