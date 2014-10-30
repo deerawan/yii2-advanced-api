22210efc1c03f247cbb41a7c9f6eb3a006e147d6
Budi (bugan) <budi@ebizu.com>
2014-10-30 08:31:55 +0700

    update model and remove .gitignore for -local files

--- api/modules/v1/models/Country.php
+++ api/modules/v1/models/Country.php
@@ -17,5 +17,12 @@ class Country extends ActiveRecord
 	public static function tableName()
 	{
 		return 'country';
-	}	
+	}
+
+    public function rules()
+    {
+        return [
+            [['code', 'name', 'population'], 'required']
+        ];
+    }	
 }
--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -5,6 +5,7 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use common\models\LoginForm;
+use api\modules\v1\models\Country;
 use yii\filters\VerbFilter;
 
 /**
@@ -63,6 +64,12 @@ class SiteController extends Controller
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
+
+        $country = new Country();
+        $country->code = 'ID';
+        $country->name = 'Indonesia';
+        $country->population = 1000;
+        $country->save();
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
