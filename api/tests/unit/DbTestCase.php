b4be64bcd74d6e64928f32da7585be2aa0985026
Budi Irawan <deerawan@gmail.com>
2015-08-12 08:29:54 +0700

    remove tests and codeception.yml

--- api/tests/unit/DbTestCase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace api\tests\unit;
-
-class DbTestCase extends \yii\codeception\DbTestCase
-{
-    public $appConfig = '@api/tests/unit/_config.php';
-}
--- api/tests/unit/TestCase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace api\tests\unit;
-
-class TestCase extends \yii\codeception\TestCase
-{
-    public $appConfig = '@api/tests/unit/_config.php';
-}
