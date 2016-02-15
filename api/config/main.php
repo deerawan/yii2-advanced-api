2172151c98a904bd72b03d83fa4aafc404f0d7dd
Marcus Freire <vinny.freire@gmail.com>
2016-02-15 00:36:44 -0300

    fix - config names fix - require undefined config

--- api/config/main.php
+++ api/config/main.php
@@ -3,8 +3,7 @@
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
     require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require(__DIR__ . '/params.php')
 );
 
 return [
